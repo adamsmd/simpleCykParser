fa1ab6d19e825f94ece7435d2c8d733bc86d71cf
Michael D. Adams <adamsmd@users.noreply.github.com>
2023-07-04 22:40:01 +0800

    Simplify Nullable.kt

--- simpleCykParser/src/main/kotlin/org/michaeldadams/simpleCykParser/parsing/Nullable.kt
+++ simpleCykParser/src/main/kotlin/org/michaeldadams/simpleCykParser/parsing/Nullable.kt
@@ -1,6 +1,7 @@
 package org.michaeldadams.simpleCykParser.parsing
 
 import org.michaeldadams.simpleCykParser.collections.DefHashMap
+import org.michaeldadams.simpleCykParser.collections.DefMap
 import org.michaeldadams.simpleCykParser.collections.QueueIterator
 import org.michaeldadams.simpleCykParser.collections.defHashMap
 import org.michaeldadams.simpleCykParser.grammar.Nonterminal
@@ -10,60 +11,31 @@ import org.michaeldadams.simpleCykParser.grammar.Symbol
 import java.util.LinkedList
 import java.util.Queue
 
-fun nullable(parseRules: ParseRules): Set<Symbol> {
-  // productionsUsing[nt][lhs].contains(p) iff p.lhs == lhs and p.rhs.contains(nt)
-  // Production p in lhs uses nt
-  val productionsUsing: DefHashMap<Nonterminal, DefHashMap<Nonterminal, MutableSet<Production>>> =
-    defHashMap { defHashMap { mutableSetOf() } }
-
-  // nonterminalsUsedBy[p].contains(nt) iff p.rhs.contains(nt) and nt is not nullable
-  // nt is in p.rhs and is not (yet) nullable
-  // A production is null if all these symbols are null
-  val nonterminalsUsedBy: DefHashMap<Production, MutableSet<Nonterminal>> =
-    defHashMap({ mutableSetOf() })
-
-  var nullable: Set<Nonterminal> = emptySet()
-  var queue: Queue<Nonterminal> = LinkedList()
-
-  // Initialize productionsUsing, nonterminalsUsedBy, nullable, and queue
-  for ((lhs, productions) in parseRules.productions.entries) {
-    if (productions.any { it.rhs.isEmpty() }) {
-      // If any production for lhs is empty, then lhs is trivially nullable
-      nullable += lhs
-      queue += lhs
-    } else {
-      // If lhs is not trivially nullable, populate productionsUsing and nonterminalsUsedBy
-      for (production in productions) {
-        try {
-          for (nt in production.rhs.map { it as Nonterminal }) {
-            productionsUsing[nt][lhs] += production
-            nonterminalsUsedBy[production] += nt
-          }
-        } catch (_: ClassCastException) {
-          // The rhs contained a Terminal, so the production will never be
-          // nullable.  Thus we bail out of working on this production by
-          // doing nothing in this 'catch' block.
-        }
-      }
+/** Compute the productions using each nonterminal. */
+fun ParseRules.productionsUsing(): Map<Symbol, Set<Production>> =
+  this.productions.values.flatMap { productions ->
+    productions.flatMap { production ->
+      production.rhs.map { it to production }
     }
-  }
+  }.groupBy { it.first }
+  .mapValues { entry -> entry.value.map { it.second }.toSet() }
 
-  // Work through the queue until it is empty
-  for (nt in QueueIterator(queue)) {
+fun ParseRules.emptyProductions(): Set<Production> =
+  this.productions.values.flatMap { productions -> productions.filter { it.rhs.isEmpty() } }.toSet()
+
+// Note that there are much more efficient algorithms for this
+fun ParseRules.nullable(): Set<Nonterminal> {
+  val uses: Map<Symbol, Set<Production>> = this.productionsUsing()
+  var nullable: Set<Nonterminal> = this.emptyProductions().map { it.lhs }.toSet()
+  var queue: Queue<Nonterminal> = LinkedList(nullable)
+
+  for (nt in QueueIterator(queue)) { // Work through the queue until it is empty
     if (nt !in nullable) { // Skip already nullable nonterminals
-      // Look at the productions that use nt
-      for ((lhs, productions) in productionsUsing[nt].entries) {
-        if (lhs !in nullable) { // Skip already nullable nonterminals
-          for (production in productions) {
-            val ntsInRhs = nonterminalsUsedBy[production]
-            // Remove nt from the nonterminals preventing the production from being nullable
-            ntsInRhs -= nt
-            // If all nonterminals have been removed, then the production is nullable
-            if (ntsInRhs.isEmpty()) {
-              nullable += lhs
-              queue += lhs
-            }
-          }
+      for (production in uses.getOrDefault(nt, emptySet())) { // For each production using nt
+        // If everything in the rhs is a nullable nonterminal, then the lhs is nullable
+        if (production.rhs.all { it is Nonterminal && it in nullable }) {
+          nullable += production.lhs // Record that the lhs is nullable
+          queue += production.lhs // The lhs being nullable may more nullables, so enqueue it for processing
         }
       }
     }
