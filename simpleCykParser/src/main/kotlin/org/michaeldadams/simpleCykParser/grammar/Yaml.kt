f20ff97144333289396b00a2a4b3840f7aaa1222
Michael D. Adams <adamsmd@users.noreply.github.com>
2023-07-06 11:19:36 +0800

    Simplify Yaml.kt

--- simpleCykParser/src/main/kotlin/org/michaeldadams/simpleCykParser/grammar/Yaml.kt
+++ simpleCykParser/src/main/kotlin/org/michaeldadams/simpleCykParser/grammar/Yaml.kt
@@ -5,11 +5,13 @@ package org.michaeldadams.simpleCykParser.grammar
 import com.charleskorn.kaml.IncorrectTypeException
 import com.charleskorn.kaml.MissingRequiredPropertyException
 import com.charleskorn.kaml.Yaml
-import com.charleskorn.kaml.YamlList
 import com.charleskorn.kaml.YamlMap
 import com.charleskorn.kaml.YamlNode
 import com.charleskorn.kaml.YamlPath
 import com.charleskorn.kaml.YamlScalar
+import com.charleskorn.kaml.yamlList
+import com.charleskorn.kaml.yamlMap
+import com.charleskorn.kaml.yamlScalar
 import kotlin.text.Regex
 import kotlin.text.toRegex
 
@@ -18,7 +20,7 @@ import kotlin.text.toRegex
  *
  * @return the map resulting from parsing the string
  */
-fun String.toYamlMap(): YamlMap = Yaml.default.parseToYamlNode(this).cast<YamlMap>()
+fun String.toYamlMap(): YamlMap = Yaml.default.parseToYamlNode(this).yamlMap
 
 /**
  * Extracts lexing rules from a [YamlMap].
@@ -27,10 +29,10 @@ fun String.toYamlMap(): YamlMap = Yaml.default.parseToYamlNode(this).cast<YamlMa
  */
 fun YamlMap.toLexRules(): LexRules {
   val map = this.toMap()
-  val whitespace: Regex = map.getYaml<YamlScalar>("whitespace").content.toRegex()
+  val whitespace: Regex = map["whitespace", this.path].yamlScalar.content.toRegex()
 
-  val terminals = map.getYaml<YamlList>("terminals").items.map {
-    it.cast<YamlMap>().toPair()
+  val terminals = map["terminals", this.path].yamlList.items.map {
+    it.yamlMap.toPair()
   }.map { LexRule(Terminal(it.first), it.second.toRegex()) }
   // TODO: regex RegexOption.COMMENTS
 
@@ -44,18 +46,21 @@ fun YamlMap.toLexRules(): LexRules {
  */
 fun YamlMap.toParseRules(): ParseRules {
   val map = this.toMap()
-  val start: String = map.getYaml<YamlScalar>("start").content
-  val productionsYaml: Map<YamlScalar, YamlNode> = map.getYaml<YamlMap>("productions").entries
+  val start: String = map["start", this.path].yamlScalar.content
+  val productionsYaml: Map<YamlScalar, YamlNode> = map["productions", this.path].yamlMap.entries
   val nonterminals: Set<String> = productionsYaml.keys.map { it.content }.toSet()
 
   val whitespaceRegex = "\\p{IsWhite_Space}+".toRegex()
   val productionsMap = productionsYaml.entries.map { entry ->
     Nonterminal(entry.key.content) to
-      entry.value.cast<YamlList>().items.map { yamlNode ->
+      entry.value.yamlList.items.map { yamlNode ->
         val (name, rhsString) = when (yamlNode) {
           is YamlMap -> yamlNode.toPair()
           is YamlScalar -> null to yamlNode.content
-          else -> incorrectType(yamlNode, "YamlMap or YamlScalar")
+          else -> throw IncorrectTypeException(
+            "Expected element to be YamlScalar or YamlMap but is ${YamlNode::class.simpleName}",
+            yamlNode.path
+          )
         }
         val rhs = rhsString
           .split(whitespaceRegex)
@@ -102,19 +107,13 @@ fun YamlMap.toGrammar(): Grammar = Grammar(this.toLexRules(), this.toParseRules(
 /* Private Helpers */
 /*********************/
 
-private fun incorrectType(node: YamlNode, type: String): Nothing =
-  throw IncorrectTypeException("Value is not a ${type}.", node.path)
-
-private inline fun <reified T : YamlNode> YamlNode.cast(): T =
-  this as? T ?: incorrectType(this, T::class.java.name)
-
-private inline fun <reified T : YamlNode> Map<String, YamlNode>.getYaml(key: String): T =
-  (this[key] ?: throw MissingRequiredPropertyException(key, YamlPath.root)).cast<T>()
+private operator fun Map<String, YamlNode>.get(key: String, path: YamlPath): YamlNode =
+  this[key] ?: throw MissingRequiredPropertyException(key, path)
 
 private fun YamlMap.toMap(): Map<String, YamlNode> = this.entries.mapKeys { it.key.content }
 
 private fun YamlMap.toPair(): Pair<String, String> {
   require(this.entries.size == 1) { "TODO" }
   val p = this.entries.toList().single()
-  return p.first.content to p.second.cast<YamlScalar>().content
+  return p.first.content to p.second.yamlScalar.content
 }
