edb0e5b131ee78a9c0d962ebe16ac6ee33fa290f
Michael D. Adams <adamsmd@users.noreply.github.com>
2023-07-06 15:22:41 +0800

    Improve serialization

--- simpleCykParser/src/main/kotlin/org/michaeldadams/simpleCykParser/parsing/Chart.kt
+++ simpleCykParser/src/main/kotlin/org/michaeldadams/simpleCykParser/parsing/Chart.kt
@@ -10,9 +10,6 @@ import com.charleskorn.kaml.YamlPath
 import com.charleskorn.kaml.YamlScalar
 import org.michaeldadams.simpleCykParser.collections.AutoMap
 import org.michaeldadams.simpleCykParser.collections.QueueSet
-import org.michaeldadams.simpleCykParser.collections.mapToYaml
-import org.michaeldadams.simpleCykParser.collections.scalarToYaml
-import org.michaeldadams.simpleCykParser.collections.setToYaml
 import org.michaeldadams.simpleCykParser.grammar.Production
 import org.michaeldadams.simpleCykParser.grammar.Symbol
 
@@ -68,7 +65,7 @@ fun Production.toPartial(consumed: Int): PartialProduction {
 class AsStringSerializer<T>(name: String) : KSerializer<T> {
   override val descriptor: SerialDescriptor = PrimitiveSerialDescriptor(name, PrimitiveKind.STRING)
 
-  override fun serialize(encoder: Encoder, value: T) {
+  override fun serialize(encoder: Encoder, value: T): Unit {
     encoder.encodeString(value.toString())
   }
 
@@ -130,47 +127,44 @@ data class Chart(val parseRules: ProcessedParseRules, val size: Int) {
       }
     }
 
-    private val toYamlImpl =
-      mapToYaml<Int, _>( // TODO: alternative to _
-        // scalarToYaml<Int>(),
-        mapToYaml<Int, _>(
-          // scalarToYaml<Int>(),
-          mapToYaml<Symbol, _>(
-            // scalarToYaml<Symbol>(),
-            setToYaml(
-              scalarToYaml<Production?>()))))
-
-    fun theSerializer() =
-      MapSerializer(
-        Int.serializer(),
+    // TODO: we don't need inner, but kotlin compilation errors if we omit inner
+    inner class Serializer() : KSerializer<Symbols> {
+      private val delegateSerializer =
         MapSerializer(
           Int.serializer(),
-          MapSerializer<Symbol, _>(
-            AsStringSerializer<Symbol>("Symbol"),
-            SetSerializer<Production?>(
-              AsStringSerializer<Production>("Production").nullable
+          MapSerializer(
+            Int.serializer(),
+            MapSerializer(
+              AsStringSerializer<Symbol>("Symbol"),
+              SetSerializer(
+                AsStringSerializer<Production>("Production").nullable
+              )
             )
           )
         )
-      )
-
-    inner class TheSerializer() : KSerializer<Symbols> {
-      private val delegateSerializer = theSerializer()
       override val descriptor = SerialDescriptor("Symbols", delegateSerializer.descriptor)
 
       override fun serialize(encoder: Encoder, value: Symbols): Unit {
-        encoder.encodeSerializableValue(delegateSerializer, this@Symbols.entries)
+        encoder.encodeSerializableValue(delegateSerializer, value.entries)
       }
 
       override fun deserialize(decoder: Decoder): Symbols {
         val map = decoder.decodeSerializableValue(delegateSerializer)
         val result = Symbols()
-        TODO() // TODO: put entries in result
+
+        for ((start, startValue) in map) {
+          for ((end, endValue) in startValue) {
+            for ((symbol, symbolValue) in endValue) {
+              for (production in symbolValue) {
+                result += Pair(start, end) to Pair(symbol, production)
+              }
+            }
+          }
+        }
+
         return result
       }
     }
-
-    fun toYaml(path: YamlPath): YamlNode = toYamlImpl(this.entries, path)
   }
 
   val productions = Productions()
@@ -180,7 +174,7 @@ data class Chart(val parseRules: ProcessedParseRules, val size: Int) {
     private val keys: AutoMap<Int, AutoMap<Int, QueueSet<PartialProduction>>> =
       AutoMap { AutoMap { QueueSet() } }
 
-    // Splitting position for a start, end and PartialProduction.  Null if PartialProduction is present but has no splitting position.
+    // Splitting position for a start, end and PartialProduction.  Null if PartialProduction is present but has no splitting position (e.g., due to empty or just being asserted).
     // TODO: rename to splits
     private val entries: AutoMap<Int, AutoMap<Int, AutoMap<PartialProduction, QueueSet<Int?>>>> =
       AutoMap { AutoMap { AutoMap { QueueSet() } } }
@@ -213,24 +207,55 @@ data class Chart(val parseRules: ProcessedParseRules, val size: Int) {
         }
       }
     }
+
+    inner class Serializer() : KSerializer<Productions> {
+      private val delegateSerializer =
+        MapSerializer(
+          Int.serializer(),
+          MapSerializer(
+            Int.serializer(),
+            MapSerializer(
+              AsStringSerializer<PartialProduction>("PartialProduction"),
+              SetSerializer(
+                Int.serializer().nullable
+              )
+            )
+          )
+        )
+      override val descriptor = SerialDescriptor("Productions", delegateSerializer.descriptor)
+
+      override fun serialize(encoder: Encoder, value: Productions): Unit {
+        encoder.encodeSerializableValue(delegateSerializer, value.entries)
+      }
+
+      override fun deserialize(decoder: Decoder): Productions {
+        val map = decoder.decodeSerializableValue(delegateSerializer)
+        val result = Productions()
+
+        for ((start, startValue) in map) {
+          for ((end, endValue) in startValue) {
+            for ((partial, partialValue) in endValue) {
+              for (split in partialValue) {
+                result += Pair(start, end) to Pair(partial, split)
+              }
+            }
+          }
+        }
+
+        return result
+      }
+    }
+
   }
 
   init {
     for (position in 0..size) {
       for (production in parseRules.nullable) {
         for (consumed in 0..production.rhs.size) {
-          productions += Pair(position, position) to Pair(production.toPartial(consumed), position)
+          val split = if (consumed == 0) null else position
+          productions += Pair(position, position) to Pair(production.toPartial(consumed), split)
         }
       }
     }
-  }
-
-  fun toYaml(path: YamlPath = YamlPath.root): YamlNode {
-    val location = path.endLocation
-    // this.symbols.entries
-    TODO()
-    // val symbolsYaml = 
-    // withPath
-    // val root = YamlMap(
   }
 }
--- simpleCykParser/src/test/kotlin/org/michaeldadams/simpleCykParser/parsing/ChartTest.kt
+++ simpleCykParser/src/test/kotlin/org/michaeldadams/simpleCykParser/parsing/ChartTest.kt
@@ -2,6 +2,9 @@ package org.michaeldadams.simpleCykParser.parsing
 
 import com.charleskorn.kaml.Yaml
 import com.charleskorn.kaml.YamlPath
+import org.michaeldadams.simpleCykParser.grammar.Terminal
+import org.michaeldadams.simpleCykParser.grammar.Production
+import org.michaeldadams.simpleCykParser.grammar.Nonterminal
 import org.michaeldadams.simpleCykParser.grammar.toParseRules
 import org.michaeldadams.simpleCykParser.grammar.toYamlMap
 import kotlin.test.Test
@@ -13,15 +16,21 @@ class ChartTest {
       productions:
         S:
           - ( S )
+          - S + S
           - X: ""
     """.trimIndent()
     val g = x.toYamlMap().toParseRules().toProcessed()
     val chart = Chart(g, 5)
-    // println(l)
-    // TODO: assertEquals(
-    //   listOf(Token("A", 1, 1), Token("B", 3, 3), Token("C", 6, 7)),
-    //   Lexer.lex(l, " A B  AA "))
-    println(chart.symbols.toYaml(YamlPath.root))
-    println(Yaml.default.encodeToString(chart.symbols.TheSerializer(), chart.symbols))
+    chart.symbols += Pair(3, 4) to Pair(Terminal("("), null)
+    chart.symbols += Pair(2, 4) to Pair(Terminal("("), Production(Nonterminal("S"), "X", emptyList()))
+    val p = PartialProduction(
+      Production(Nonterminal("S"), null, listOf(Nonterminal("S"), Terminal("+"), Nonterminal("S"))),
+      2
+    )
+
+    chart.productions += Pair(2, 4) to Pair(p, null)
+    chart.productions += Pair(1, 4) to Pair(p.consume()!!, null)
+    println(Yaml.default.encodeToString(chart.symbols.Serializer(), chart.symbols))
+    println(Yaml.default.encodeToString(chart.productions.Serializer(), chart.productions))
   }
 }
