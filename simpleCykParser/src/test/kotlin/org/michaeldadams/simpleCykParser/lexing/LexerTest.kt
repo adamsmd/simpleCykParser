c5ba98bae57e3ff1cfb033783fc7aa8fab7814b7
Michael D. Adams <adamsmd@users.noreply.github.com>
2023-07-08 11:51:37 +0800

    Cleanup LexerTest.kt

--- simpleCykParser/src/test/kotlin/org/michaeldadams/simpleCykParser/lexing/LexerTest.kt
+++ simpleCykParser/src/test/kotlin/org/michaeldadams/simpleCykParser/lexing/LexerTest.kt
@@ -8,36 +8,27 @@ import kotlin.test.assertEquals
 import kotlin.test.assertFailsWith
 
 class LexerTest {
-  @Test fun testTokenConstruction(): Unit {
-    assertFailsWith(IllegalArgumentException::class) { Token(Terminal("A"), listOf()) }
-    assertFailsWith(IllegalArgumentException::class) { Token(Terminal("A"), listOf(null)) }
-    Token(Terminal("A"), listOf(MatchGroup("A", IntRange(1, 1))))
-  }
-
-  @Test fun testLexing(): Unit {
-    val x = """
+  @Test fun testLex(): Unit {
+    val l = """
       whitespace: "\\s+"
       terminals:
         - A: "A"
         - B: "B B"
         - X: "[A-Z]+"
         - Y: "(?x) \\d+ ( \\.\\d+ )?"
-    """.trimIndent()
-
-    val y = x.toYamlMap()
-    val l = y.toLexRules()
+    """.trimIndent().toYamlMap().toLexRules()
 
     val expectedTokens = listOf(
       Token(Terminal("A"), listOf(MatchGroup("A", IntRange(1, 1)))),
       Token(Terminal("B"), listOf(MatchGroup("B B", IntRange(3, 5)))),
       Token(Terminal("X"), listOf(MatchGroup("AA", IntRange(8, 9)))),
-      Token(Terminal("Y"), listOf(MatchGroup("123", IntRange(11, 13)), null)),
+      Token(Terminal("Y"), listOf(MatchGroup("123", IntRange(10, 12)), null)),
       Token(
         Terminal("Y"),
         listOf(MatchGroup("123.45", IntRange(15, 20)), MatchGroup(".45", IntRange(18, 20)))
       )
     )
 
-    assertEquals(Pair(23, expectedTokens), lex(l, " A B B  AA 123 123.45  "))
+    assertEquals(Pair(23, expectedTokens), lex(l, " A B B  AA123  123.45  "))
   }
 }
