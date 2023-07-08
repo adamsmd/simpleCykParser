8f7563e665b3d381355932ac2ad337ec52d0e26e
Michael D. Adams <adamsmd@users.noreply.github.com>
2023-07-08 11:55:28 +0800

    Move functions from ProcessedParseRules.kt to Properties.kt

--- simpleCykParser/src/main/kotlin/org/michaeldadams/simpleCykParser/parsing/ProcessedParseRules.kt
+++ simpleCykParser/src/main/kotlin/org/michaeldadams/simpleCykParser/parsing/ProcessedParseRules.kt
@@ -7,56 +7,8 @@ import org.michaeldadams.simpleCykParser.grammar.Nonterminal
 import org.michaeldadams.simpleCykParser.grammar.ParseRules
 import org.michaeldadams.simpleCykParser.grammar.Production
 import org.michaeldadams.simpleCykParser.grammar.Symbol
-import org.michaeldadams.simpleCykParser.grammar.productionsUsing
-
-/**
- * TODO.
- *
- * @receiver TODO
- * @return TODO
- */
-fun ParseRules.initialUses(): Map<Symbol, Set<Production>> =
-  this.productionMap.values.flatten()
-    .filter { it.rhs.isNotEmpty() }
-    .groupBy { it.rhs.first().second }
-    .mapValues { it.value.toSet() }
-
-/**
- * TODO.
- *
- * @receiver TODO
- * @return TODO
- */
-fun ParseRules.epsilons(): Set<Production> =
-  this.productionMap.values.flatten().filter { it.rhs.isEmpty() }.toSet()
-
-/**
- * TODO.
- *
- * TODO: Note that there are much more efficient algorithms for this.
- *
- * @receiver TODO
- * @return TODO
- */
-fun ParseRules.nullable(): Set<Production> {
-  val uses: Map<Symbol, Set<Production>> = this.productionsUsing()
-  // TODO: Note that productions serves as both a record (Set) and worklist (Queue)
-  var productions: QueueSet<Production> = this.epsilons().toCollection(QueueSet())
-  var nonterminals: Set<Nonterminal> = productions.map { it.lhs }.toSet()
-
-  for (workitem in productions) { // Work through the queue until it is empty
-    // For each use of workitems's nonterminal
-    for (production in uses.getOrDefault(workitem.lhs, emptySet())) {
-      // If rhs is only nullable nonterminals, then the production is nullable
-      if (production.rhs.all { it.second is Nonterminal && it.second in nonterminals }) {
-        productions += production // Enqueue the nullable production
-        nonterminals += production.lhs // Record the nullable nonterminal
-      }
-    }
-  }
-
-  return productions.toSet()
-}
+import org.michaeldadams.simpleCykParser.grammar.initialUses
+import org.michaeldadams.simpleCykParser.grammar.nullable
 
 /**
  * TODO.
