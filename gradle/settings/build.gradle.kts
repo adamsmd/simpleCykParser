971961929bb39d1b1e35aafe461dcfef70663933
Michael D. Adams <adamsmd@users.noreply.github.com>
2023-07-10 13:06:35 +0800

    Upgrade Kotlin to 1.8

--- gradle/settings/build.gradle.kts
+++ gradle/settings/build.gradle.kts
@@ -25,10 +25,10 @@ dependencies {
   implementation("com.github.ben-manes:gradle-versions-plugin:0.42.0")
 
   // Documentation
-  implementation("org.jetbrains.dokka:dokka-gradle-plugin:1.7.20")
+  implementation("org.jetbrains.dokka:dokka-gradle-plugin:1.8.20")
 
   // Kotlin Plugin
-  implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.7.10")
+  implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.8.22")
 
   // Linting
   implementation("io.gitlab.arturbosch.detekt:detekt-gradle-plugin:1.22.0")
